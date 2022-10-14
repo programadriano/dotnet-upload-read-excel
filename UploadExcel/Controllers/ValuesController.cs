f7c68bb27bfeb2fb4591cef0c5c77f50b28b4876
Thiago S Adriano <tadriano.net@gmail.com>
2022-10-13 23:35:34 -0300

    first commit

---
+++ UploadExcel/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using ExcelDataReader;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using UploadExcel.Models;
+
+namespace UploadExcel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post(IFormFile file)
+        {
+            List<Exemplo> examples = new List<Exemplo>();
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            using (var stream = new MemoryStream())
+            {
+                file.CopyTo(stream);
+                stream.Position = 0;
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var result = reader.AsDataSet();
+                    DataRowCollection dt = result.Tables[0].Rows;
+
+
+                    //for (int i = 1; i < dt.Count; i++)
+                    //{
+                    //    for (int j = 1; j < dt.Count; j++)
+                    //    {
+
+                    //    }
+                    //}
+
+                    while (reader.Read()) //Each row of the file
+                    {
+                        examples.Add(new Exemplo(reader.GetValue(0).ToString(), reader.GetValue(1).ToString(), reader.GetValue(2).ToString(), reader.GetValue(3).ToString()));
+                    }
+                }
+            }
+
+            return Ok(examples);
+        }
+
+
+    }
+}
---
+++ UploadExcel/Models/Exemplo.cs
@@ -0,0 +1,18 @@
+﻿namespace UploadExcel.Models
+{
+    public class Exemplo
+    {
+        public Exemplo(string valorA, string valorB, string valorC, string valorD)
+        {
+            ValorA = valorA;
+            ValorB = valorB;
+            ValorC = valorC;
+            ValorD = valorD;
+        }
+
+        public string ValorA { get; private set; }
+        public string ValorB { get; private set; }
+        public string ValorC { get; private set; }
+        public string ValorD { get; private set; }
+    }
+}
